4399eb8c6987663ff397e71dde3a36dfa3255852
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-04 21:58:03 +0100

    First app created

---
+++ ReactNativeApp/App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+         <Text> hola </Text>
+         <Text> prueba2  hechaaa</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
