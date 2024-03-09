e39b9bd550e14fd411845336af05b81445a98523
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-09 17:04:56 +0100

    Login working

--- ReactNativeApp/src/App.tsx
+++ ReactNativeApp/src/App.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from './Home';
-import LoginScreen from './LoginPage';
-
-const Stack = createStackNavigator();
+import React from 'react';
+import { Text, View } from 'react-native';
+import Navigation from './components/Navigation';
 
 function App(): React.JSX.Element {
 
   return (
+    
+    /*<View>
+      <Text> hola, sigo funcionando </Text>
+    </View>*/
+    
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login"> 
-        <Stack.Screen name="Login" component={LoginScreen} /> 
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Navigation />
     </NavigationContainer>
   );
 }
