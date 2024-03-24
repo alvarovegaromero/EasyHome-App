af0235cae6ddc88a2772c76d6623a97064da1acf
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-24 19:36:24 +0100

    Added remote server

--- ReactNativeApp/src/config.ts
+++ ReactNativeApp/src/config.ts
@@ -1,7 +1,7 @@
 const BASE_URL_LOCAL = 'http://10.0.2.2:8000'; 
-const BASE_URL_REMOTE = '';
+const BASE_URL_REMOTE = 'http://192.168.1.50:8000';
 
-const BASE_URL = BASE_URL_LOCAL; //Switch when needed
+const BASE_URL = BASE_URL_REMOTE; //Switch when needed
 
 export { BASE_URL };
   
