55a37719c051e0322a4726b51e34e176a6b4bf3e
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-24 17:52:55 +0100

    Update config.ts

--- ReactNativeApp/src/config.ts
+++ ReactNativeApp/src/config.ts
@@ -1,4 +1,4 @@
-const BASE_URL_LOCAL = 'http://10.0.2.2:8000'; //
+const BASE_URL_LOCAL = 'http://10.0.2.2:8000'; 
 const BASE_URL_REMOTE = '';
 
 const BASE_URL = BASE_URL_LOCAL; //Switch when needed
