2680c10573da5bc095e71b83284d7c7679da0759
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-28 19:30:53 +0100

    WIP: Applying style to Login Page

--- ReactNativeApp/src/screens/LoginScreen.tsx
+++ ReactNativeApp/src/screens/LoginScreen.tsx
@@ -3,6 +3,8 @@ import { Alert, Button, GestureResponderEvent, SafeAreaView, ScrollView, Text, T
 import { useNavigation } from '@react-navigation/native';
 import { BASE_URL } from '../config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import stylesLoginScreen from '../styles/stylesLoginScreen';
+import generalStyles from '../styles/styles';
 
 const LoginScreen: React.FC = () => {
     const navigation = useNavigation();
@@ -40,23 +42,29 @@ const LoginScreen: React.FC = () => {
     };
 
     return (
-        <SafeAreaView>
-            <ScrollView>
-                <Text>Username:</Text>
-                <TextInput
-                    value={username}
-                    onChangeText={text => setUsername(text)}
-                />
-                <Text>Password:</Text>
-                <TextInput
-                    secureTextEntry
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                />
-                <Button title="Login" onPress={handleLoginSubmit} />
-
-                <Text>Don't you have an account? Register now!</Text>
-                <Button title="Register" onPress={() => navigation.navigate('Register' as never)} />
+        <SafeAreaView style={generalStyles.defaultSafeAreaView}>
+            <ScrollView contentContainerStyle={generalStyles.defaultScrollView}>
+                <View style={stylesLoginScreen.container}> 
+                    <Text> Foto </Text>
+                </View>
+                <View style={stylesLoginScreen.container}> 
+                    <Text>Username:</Text>
+                    <TextInput
+                        value={username}
+                        onChangeText={text => setUsername(text)}
+                    />
+                    <Text>Password:</Text>
+                    <TextInput
+                        secureTextEntry
+                        value={password}
+                        onChangeText={text => setPassword(text)}
+                    />
+                    <Button title="Login" onPress={handleLoginSubmit} />
+                </View>
+                <View style={stylesLoginScreen.container}> 
+                    <Text>Don't you have an account? Register now!</Text>
+                    <Button title="Register" onPress={() => navigation.navigate('Register' as never)} />
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
