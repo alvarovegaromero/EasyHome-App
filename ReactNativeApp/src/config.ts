6f47d25164d1b3bd6ac492087a0ce6b87ab2c61c
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-31 23:00:25 +0200

    WIP: connecting with proxy server

--- ReactNativeApp/src/config.ts
+++ ReactNativeApp/src/config.ts
@@ -1,11 +1,11 @@
 import packageJson from '../package.json';
 
-const BASE_URL_LOCAL = 'http://10.0.2.2:8000'; 
-const BASE_URL_REMOTE = 'http://192.168.1.50:8000';
+const BASE_URL_LOCAL = 'https://10.0.2.2'; 
+const BASE_URL_REMOTE = 'https://192.168.1.50';
 
 const BASE_URL = BASE_URL_REMOTE; //Switch when needed
 
-const APP_VERSION = packageJson.version;
+const APP_VERSION = packageJson.version; //Maybe not good in production
 
 export { BASE_URL, APP_VERSION };
   
