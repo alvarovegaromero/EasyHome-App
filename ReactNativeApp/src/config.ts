ca5820fa42c7b0725bd806637cea951b8ca845f8
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-02 23:03:20 +0200

    Update config.ts

--- ReactNativeApp/src/config.ts
+++ ReactNativeApp/src/config.ts
@@ -1,10 +1,10 @@
 import packageJson from '../package.json';
 
 const BASE_URL_LOCAL = 'https://10.0.2.2'; 
-const BASE_URL_REMOTE = 'https://192.168.1.50';
-const BASE_URL_HOTSPOT= 'https://192.168.78.213';
+const BASE_URL_NORMAL_NETWORK = 'https://192.168.1.50';
+const BASE_URL_HOTSPOT_NETWORK= 'https://192.168.78.213';
 
-const BASE_URL = BASE_URL_HOTSPOT; //Switch when needed
+const BASE_URL = BASE_URL_NORMAL_NETWORK; //Switch when needed
 
 const APP_VERSION = packageJson.version; //Maybe not good in production
 
