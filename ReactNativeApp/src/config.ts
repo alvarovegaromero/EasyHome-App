f01aeea0d80f641720a9161e472fedddc83e3f37
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-02 23:02:08 +0200

    Using mobile as hotspot

--- ReactNativeApp/src/config.ts
+++ ReactNativeApp/src/config.ts
@@ -2,8 +2,9 @@ import packageJson from '../package.json';
 
 const BASE_URL_LOCAL = 'https://10.0.2.2'; 
 const BASE_URL_REMOTE = 'https://192.168.1.50';
+const BASE_URL_HOTSPOT= 'https://192.168.78.213';
 
-const BASE_URL = BASE_URL_REMOTE; //Switch when needed
+const BASE_URL = BASE_URL_HOTSPOT; //Switch when needed
 
 const APP_VERSION = packageJson.version; //Maybe not good in production
 
