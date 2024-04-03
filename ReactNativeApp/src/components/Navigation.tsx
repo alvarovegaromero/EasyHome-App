a0b0749c45c29f741bc8f194f4f27e48134bed5a
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-03 23:00:53 +0200

    ProfileScreen added to Stack

--- ReactNativeApp/src/components/Navigation.tsx
+++ ReactNativeApp/src/components/Navigation.tsx
@@ -3,7 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
-import RegisterScreen from '../screens/RegisterScreen'; //TODO: Implement
+import RegisterScreen from '../screens/RegisterScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const AuthStack = () => {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
