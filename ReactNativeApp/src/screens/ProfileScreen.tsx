d91a2d30b2351b80fcdf8a511f1fd1347dff832b
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-03 23:01:08 +0200

    WIP: ProfileScreen page

--- ReactNativeApp/src/screens/ProfileScreen.tsx
+++ ReactNativeApp/src/screens/ProfileScreen.tsx
@@ -1,34 +1,27 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-interface ProfileScreenProps {
-    username: string;
-    email: string;
-    firstName?: string;
-    lastName?: string;
-    navigation: any; // Replace with the appropriate navigation type
-}
+const ProfileScreen: React.FunctionComponent = () => {
+    const navigation = useNavigation();
 
-const ProfileScreen: React.FC<ProfileScreenProps> = ({
-    username,
-    email,
-    firstName,
-    lastName,
-    navigation,
-}) => {
+    
     const handleGoBack = () => {
         navigation.goBack();
     };
 
     return (
         <View>
-            <Text>Username: {username}</Text>
-            <Text>Email: {email}</Text>
-            {firstName && <Text>First Name: {firstName}</Text>}
-            {lastName && <Text>Last Name: {lastName}</Text>}
-            <Button title="Go Back" onPress={handleGoBack} />
+            <Text>Username: username</Text>
+            <Button title="Go Back" onPress={handleGoBack} /> 
         </View>
     );
 };
+
+/* 
+            <Text>Email: {email}</Text>
+            {firstName && <Text>First Name: {firstName}</Text>}
+            {lastName && <Text>Last Name: {lastName}</Text>}"""
+            */
 
 export default ProfileScreen;
