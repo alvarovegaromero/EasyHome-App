b3e29ffd87d6be606db7c0d3b044c0fd1990f5cc
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-06 18:51:05 +0200

    Page renamed

---
+++ ReactNativeApp/src/screens/ResetPasswordScreen.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, ScrollView, Text } from "react-native";
+
+
+const ResetPasswordScreen: React.FunctionComponent = () => {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <Text>Reset Password Screen</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default ResetPasswordScreen;
