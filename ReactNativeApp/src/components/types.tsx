629cd11c6feef2724e2d06a972e763c1d2eb9cdb
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-06 19:10:22 +0200

    Update types.tsx

--- ReactNativeApp/src/components/types.tsx
+++ ReactNativeApp/src/components/types.tsx
@@ -1,1 +1,7 @@
-export type MyStackParamList = { LoginScreen: undefined, RegisterScreen: undefined, ResetPasswordScreen: undefined, HomeScreen: { username: string }, ProfileScreen: undefined }
+export type MyStackParamList = { 
+    LoginScreen: undefined, 
+    RegisterScreen: undefined, 
+    ResetPasswordScreen: undefined, 
+    HomeScreen: { username: string }, 
+    ProfileScreen: undefined 
+}
