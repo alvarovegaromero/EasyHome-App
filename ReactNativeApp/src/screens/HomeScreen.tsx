1d1d2bba52a6c0b9e03a0d86cfe6bc3d5887ccb6
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-15 19:26:41 +0200

    logic from HomeScreen moved to its controller

--- ReactNativeApp/src/screens/HomeScreen.tsx
+++ ReactNativeApp/src/screens/HomeScreen.tsx
@@ -1,47 +1,23 @@
 import React from 'react';
-import { BASE_URL } from '../config';
-import { Alert, Button, SafeAreaView, ScrollView, Text } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Button, SafeAreaView, ScrollView, Text } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { MyStackParamsList } from '../components/types';
+import useHomeController from './hooks/useHomeController';
 
 type props = StackScreenProps<MyStackParamsList, 'HomeScreen'>;
-
-const HomeScreen: React.FunctionComponent<props> = ({navigation, route}: props) => {
+                                                
+const HomeScreen: React.FunctionComponent<props> = ({route}: props) => { 
+    //we can use also use: {navigation, route} : props
     const username = route.params.username;
 
-    const handleLogout = async () => {
-        try {
-            const token = await AsyncStorage.getItem('token');
-    
-            const response = await fetch(BASE_URL+'/api/users/logout', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Token ${token}`,
-                },
-            });
-
-            if (response.ok) {
-                await AsyncStorage.removeItem('token');
-                navigation.navigate('LoginScreen' as never);
-            } else {
-                return response.json().then(({ error }) => {
-                    Alert.alert(`Error ${response.status}`, error);
-                    throw new Error(`${response.status} - ${error}`, );
-                });
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+    const { handleLogout, navigateProfileScreen } = useHomeController();
 
     return (
         <SafeAreaView>
             <ScrollView>
                 <Text> Hola {username}, you're logged in!!!! </Text>
                 <Button title="Logout" onPress={handleLogout} />
-                <Button title="Go to Profile" onPress={() => navigation.navigate('ProfileScreen' as never)} />
+                <Button title="Go to Profile" onPress={navigateProfileScreen} />
             </ScrollView>
         </SafeAreaView>
     );
---
+++ ReactNativeApp/src/screens/hooks/useHomeController.tsx
@@ -0,0 +1,42 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BASE_URL } from '../../config';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const useHomeController = () => {
+    const navigation = useNavigation();
+
+    const handleLogout = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+    
+            const response = await fetch(BASE_URL+'/api/users/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                await AsyncStorage.removeItem('token');
+                navigation.navigate('LoginScreen' as never);
+            } else {
+                return response.json().then(({ error }) => {
+                    Alert.alert(`Error ${response.status}`, error);
+                    throw new Error(`${response.status} - ${error}`, );
+                });
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const navigateProfileScreen = () => {
+        navigation.navigate('ProfileScreen' as never);
+    }; 
+    return { handleLogout, navigateProfileScreen };
+
+};
+
+export default useHomeController;
