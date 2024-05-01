8bd8f6bf2c41db25e8fc176b2a2df71ce602727c
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-01 16:36:41 +0200

    Added group fetch in GroupScreen

--- ReactNativeApp/src/screens/postLogin/Group/GroupHomeScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/GroupHomeScreen.tsx
@@ -1,6 +1,9 @@
 import { Text, View } from "react-native";
+import useGroupHomeController from "./hooks/useGroupHomeController";
 
 const GroupHomeScreen: React.FunctionComponent = () => {
+    const { } = useGroupHomeController();
+
     return (
         <View>
             <Text>Group Home Screen</Text>
---
+++ ReactNativeApp/src/screens/postLogin/Group/hooks/useGroupHomeController.tsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect } from "react";
+import { GroupContext } from "../../../../contexts/GroupContext";
+import { BASE_URL } from "../../../../config";
+
+const useGroupHomeController = () => {
+
+    const { groupId } = useContext(GroupContext)
+
+    useEffect(() => {
+        fetchGroupData();
+    }, []);
+
+    const fetchGroupData = () => {
+        fetch(BASE_URL+'/api/groups/'+groupId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(({ error }) => {
+                    throw new Error(`${response.status} - ${error}`);
+                });
+            }
+            else
+                return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
+
+
+    return {};
+};
+
+export default useGroupHomeController;
