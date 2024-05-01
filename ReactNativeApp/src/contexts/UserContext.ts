8a7f14e3b8a56a7f891032739ece6141135799b9
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-01 17:57:59 +0200

    Added username to context

--- ReactNativeApp/src/contexts/UserContext.ts
+++ ReactNativeApp/src/contexts/UserContext.ts
@@ -3,9 +3,13 @@ import React from 'react';
 interface UserContextProps {
     id: string;
     setId: (id: string) => void;
+    contextUsername: string;
+    setContextUsername: (username: string) => void;
 }
 
 export const UserContext = React.createContext<UserContextProps>({
     id: '',
     setId: () => {},
+    contextUsername: '',
+    setContextUsername: () => {},
 });
--- ReactNativeApp/src/hooks/useUserContext.tsx
+++ ReactNativeApp/src/hooks/useUserContext.tsx
@@ -3,12 +3,15 @@ import { useState } from 'react';
 interface UseUserHook {
     id: string;
     setId: (id: string) => void;
+    contextUsername: string;
+    setContextUsername: (username: string) => void;
 }
 
 const useUser = (): UseUserHook => {
     const [id, setId] = useState('');
+    const [contextUsername, setContextUsername] = useState('');
 
-    return { id, setId };
+    return { id, setId, contextUsername, setContextUsername};
 };
 
 export default useUser;
