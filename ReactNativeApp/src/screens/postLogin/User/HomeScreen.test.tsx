e0e166d353e7fe0ca0c4aaaa082b87347b01110b
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-05 11:52:39 +0200

    Tests for HomeScreen updated

--- ReactNativeApp/src/screens/postLogin/User/HomeScreen.test.tsx
+++ ReactNativeApp/src/screens/postLogin/User/HomeScreen.test.tsx
@@ -1,34 +1,37 @@
 import { render, fireEvent } from '@testing-library/react-native';
 import HomeScreen from './HomeScreen';
 import useHomeController from './hooks/useHomeController';
-import { RouteProp } from '@react-navigation/native';
-import { MyStackParamsList } from '../../../components/types';
-import { StackNavigationProp } from '@react-navigation/stack';
+
+var mockDialogVisible = false;
 
 jest.mock('./hooks/useHomeController', () => {
     const handleLogout = jest.fn();
     const navigateProfileScreen = jest.fn();
+    const showDialog = jest.fn();
+    const closeDialog = jest.fn();
+    const setJoinCode = jest.fn();
+    const joinGroup = jest.fn();
+    const navigateGroupHomeScreen = jest.fn();
+    const navigateCreateGroupScreen = jest.fn();
 
     return () => ({
+        username: 'myUsername',
+        groups: [{ id: '1', name: 'Group 1' }, { id: '2', name: 'Group 2' }],
         handleLogout,
+        showDialog,
+        closeDialog,
+        dialogVisible: mockDialogVisible,
+        setJoinCode,
+        joinGroup,
+        navigateGroupHomeScreen,
         navigateProfileScreen,
+        navigateCreateGroupScreen,
     });
 });
 
 const renderScreen = () => {
-    const [route, navigation] = [{
-        key: 'HomeScreen',
-        name: 'HomeScreen',
-        params: { username: 'myUsername' },
-    }, {
-        navigate: jest.fn(),
-    }] as const;
-
     return render(
-        <HomeScreen 
-            route={route as RouteProp<MyStackParamsList, "HomeScreen">} 
-            navigation={navigation as unknown as StackNavigationProp<MyStackParamsList, "HomeScreen">} 
-        />
+        <HomeScreen/>
     );
 };
 
@@ -36,25 +39,57 @@ describe('HomeScreen', () => {
     it('should render all components', () => {
         const { getByText, getByTestId } = renderScreen();
 
-        expect(getByText("Hola myUsername, you're logged in!!!!")).toBeTruthy();
+        expect(getByText("Hello myUsername!")).toBeTruthy();
+        expect(getByText("Select the group you want to see:")).toBeTruthy();
+        expect(getByText("Your Groups:")).toBeTruthy();
+        expect(getByText("Group 1")).toBeTruthy();
+        expect(getByText("Group 2")).toBeTruthy();
 
-    
         expect(getByTestId('LogoutButton')).toBeTruthy();
         expect(getByTestId('ProfileButton')).toBeTruthy();
+        expect(getByTestId('JoinGroupButton')).toBeTruthy();
+        expect(getByTestId('CreateGroupButton')).toBeTruthy();
     });
 
     it('should call the correct functions when buttons are pressed', () => {
         const { getByTestId } = renderScreen();
     
-        const { handleLogout, navigateProfileScreen } = useHomeController();
+        const { handleLogout, navigateProfileScreen, showDialog, navigateCreateGroupScreen } = useHomeController();
 
         expect(handleLogout).not.toHaveBeenCalled();
         expect(navigateProfileScreen).not.toHaveBeenCalled();
+        expect(showDialog).not.toHaveBeenCalled();
+        expect(navigateCreateGroupScreen).not.toHaveBeenCalled();
         
         fireEvent.press(getByTestId('LogoutButton'));
         fireEvent.press(getByTestId('ProfileButton'));
+        fireEvent.press(getByTestId('JoinGroupButton'));
+        fireEvent.press(getByTestId('CreateGroupButton'));
         
         expect(handleLogout).toHaveBeenCalled();
         expect(navigateProfileScreen).toHaveBeenCalled();
+        expect(showDialog).toHaveBeenCalled();
+        expect(navigateCreateGroupScreen).toHaveBeenCalled();
+    });
+
+    it('should handle group join dialog correctly', () => {
+        mockDialogVisible = true;
+        const { getByText } = renderScreen();
+    
+        // Verify that the dialog is rendered correctly
+        expect(getByText('Join Group')).toBeTruthy();
+        expect(getByText('Enter the join code for the group you want to join.')).toBeTruthy();
+    
+        const { closeDialog, joinGroup } = useHomeController();
+    
+        expect(closeDialog).not.toHaveBeenCalled();
+        expect(joinGroup).not.toHaveBeenCalled();
+    
+        // Simulate pressing the Cancel and Join buttons
+        fireEvent.press(getByText('Cancel'));
+        fireEvent.press(getByText('Join'));
+    
+        expect(closeDialog).toHaveBeenCalled();
+        expect(joinGroup).toHaveBeenCalled();
     });
 });
