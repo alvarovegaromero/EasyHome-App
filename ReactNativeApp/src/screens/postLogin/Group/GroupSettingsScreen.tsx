944be3f1838bcb271e6c592861b422f1823ec8af
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-12 00:33:48 +0200

    WIP: adding styles to GroupSettingsController

--- ReactNativeApp/src/screens/postLogin/Group/GroupSettingsScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/GroupSettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Button, Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import generalStyles from "../../../styles/styles";
 import useGroupSettingsController from "./hooks/useGroupSettingsController";
 import Dialog from "react-native-dialog";
@@ -13,42 +13,81 @@ const GroupSettingsScreen: React.FunctionComponent = () => {
         <SafeAreaView style={generalStyles.defaultSafeAreaView}>
             <ScrollView style={generalStyles.defaultScrollView}>
                 <View style={generalStyles.defaultContainerScreen}>
-                    <Button 
-                        title="Leave group"
-                        onPress={confirmAndLeaveGroup}
-                        accessibilityLabel='Leave group button'
-                        testID='LeaveGroupButton'
-                    />
-                    {isOwner && (
-                        <Button 
-                            title="Delete group"
-                            onPress={confirmAndDeleteGroup}
-                            accessibilityLabel='Delete group button'
-                            testID='DeleteGroupButton'
-                        />
-                    )}
-                    <Button 
-                        title="Generate join code"
-                        onPress={generateJoinCode}
-                        accessibilityLabel='Generate join code button'
-                        testID='GenerateJoinCodeButton'
-                    />
-                    <Button
-                        title="Go back to Group Home"
-                        onPress={navigateGroupHome}
-                        accessibilityLabel='Go back to Group Home button'
-                        testID='GoBackToGroupHomeButton'
-                    />
                     
-                    <View style={stylesGroupSettingsController.containerUsers}>
-                        {groupUsers.map((user : User) => (
-                            <TouchableOpacity 
-                                key={user.id} 
-                                onPress={() => console.log('User pressed:', user.username)}
-                            >
-                                <Text>{user.username}</Text>
-                            </TouchableOpacity>
-                        ))}   
+                    <View style={stylesGroupSettingsController.containerUsersPart}>
+                        <Text style={generalStyles.defaultHeader}>Members:</Text>
+                        <View style={stylesGroupSettingsController.containerUsers}>
+                            {groupUsers.map((user : User) => (
+                                <TouchableOpacity 
+                                    key={user.id} 
+                                    onPress={() => console.log('User pressed:', user.username)}
+                                    style={stylesGroupSettingsController.listButton}
+                                >
+                                    <View style={stylesGroupSettingsController.contentListButton}>
+                                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                            <Text style={stylesGroupSettingsController.textList}>{user.username}</Text>
+                                            {user.is_owner && (
+                                                <Image 
+                                                    source={require('../../../../assets/images/crownIcon.png')} 
+                                                    style={stylesGroupSettingsController.ownerCrownIconList}
+                                                />
+                                            )}
+                                        </View>
+                                        {isOwner && (
+                                            <Image 
+                                                source={require('../../../../assets/images/crossIcon.png')} 
+                                                style={stylesGroupSettingsController.crossIconList}
+                                            />
+                                        )}
+                                    </View>
+                                </TouchableOpacity>
+                            ))}   
+                        </View>
+                    </View>
+
+                    <View style={stylesGroupSettingsController.containerButtons}>
+                        <View style={stylesGroupSettingsController.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Leave group"
+                                    onPress={confirmAndLeaveGroup}
+                                    accessibilityLabel='Leave group button'
+                                    testID='LeaveGroupButton'
+                                />
+                            </View>
+                        </View>
+                        {isOwner && (
+                        <View style={stylesGroupSettingsController.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Delete group"
+                                    onPress={confirmAndDeleteGroup}
+                                    accessibilityLabel='Delete group button'
+                                    testID='DeleteGroupButton'
+                                />
+                            </View>
+                        </View>
+                        )}
+                        <View style={stylesGroupSettingsController.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Get join code"
+                                    onPress={generateJoinCode}
+                                    accessibilityLabel='Get join code button'
+                                    testID='GetJoinCodeButton'
+                                />
+                            </View>
+                        </View>
+                        <View style={stylesGroupSettingsController.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button
+                                    title="Go back to Group Home"
+                                    onPress={navigateGroupHome}
+                                    accessibilityLabel='Go back to Group Home button'
+                                    testID='GoBackToGroupHomeButton'
+                                />
+                            </View>
+                        </View>
                     </View>
 
                     <Dialog.Container visible={dialogVisible}>
--- ReactNativeApp/src/styles/stylesGroupSettingsController.ts
+++ ReactNativeApp/src/styles/stylesGroupSettingsController.ts
@@ -1,12 +1,47 @@
 import { StyleSheet } from 'react-native';
+import generalStyles from './styles';
 
 
 const stylesGroupSettingsController = StyleSheet.create({
+    containerUsersPart: {
+        ...generalStyles.defaultContainerHeader,
+    },
     containerUsers: {
         borderColor: 'black',
         borderWidth: 1,
-        borderRadius: 2,        
+        borderRadius: 5,        
         height: 300,
+        paddingTop: 12,
+    },
+    containerButtons: {
+    },
+    containerButton: {
+        ...generalStyles.defaultContainerButton,
+        marginTop: 5,
+        marginBottom: 10,
+    },
+    listButton: {
+        padding: 2,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10,
+        height: 40,
+    },
+    contentListButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    ownerCrownIconList: {
+        height: 25,
+        width: 25,
+    },
+    crossIconList: {
+        height: 20,
+        width: 20,
+    },
+    textList: {
+        fontSize: 20,
+        marginRight: 10,
     },
 });
 
