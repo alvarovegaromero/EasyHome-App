0282fb3b1852c3b9f7e6b72912d545e0079b3047
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-11 19:58:20 +0200

    WIP: add styles to GroupSettingsScreens

--- ReactNativeApp/src/screens/postLogin/Group/GroupSettingsScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/GroupSettingsScreen.tsx
@@ -2,7 +2,8 @@ import { Button, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "
 import generalStyles from "../../../styles/styles";
 import useGroupSettingsController from "./hooks/useGroupSettingsController";
 import Dialog from "react-native-dialog";
-
+import stylesGroupSettingsController from "../../../styles/stylesGroupSettingsController";
+import { User } from "./types";
 
 const GroupSettingsScreen: React.FunctionComponent = () => {
     const {confirmAndLeaveGroup, confirmAndDeleteGroup, generateJoinCode, dialogVisible, 
@@ -11,52 +12,54 @@ const GroupSettingsScreen: React.FunctionComponent = () => {
     return (
         <SafeAreaView style={generalStyles.defaultSafeAreaView}>
             <ScrollView style={generalStyles.defaultScrollView}>
-                <Button 
-                    title="Leave group"
-                    onPress={confirmAndLeaveGroup}
-                    accessibilityLabel='Leave group button'
-                    testID='LeaveGroupButton'
-                />
-                {isOwner && (
+                <View style={generalStyles.defaultContainerScreen}>
                     <Button 
-                        title="Delete group"
-                        onPress={confirmAndDeleteGroup}
-                        accessibilityLabel='Delete group button'
-                        testID='DeleteGroupButton'
+                        title="Leave group"
+                        onPress={confirmAndLeaveGroup}
+                        accessibilityLabel='Leave group button'
+                        testID='LeaveGroupButton'
                     />
-                )}
-                <Button 
-                    title="Generate join code"
-                    onPress={generateJoinCode}
-                    accessibilityLabel='Generate join code button'
-                    testID='GenerateJoinCodeButton'
-                />
-                <Button
-                    title="Go back to Group Home"
-                    onPress={navigateGroupHome}
-                    accessibilityLabel='Go back to Group Home button'
-                    testID='GoBackToGroupHomeButton'
-                />
-                
-                <View>
-                    {groupUsers.map((user, index) => (
-                        <TouchableOpacity 
-                            key={user.id} 
-                            onPress={() => console.log('User pressed:', user.username)}
-                        >
-                            <Text>{user.username}</Text>
-                        </TouchableOpacity>
-                    ))}   
-                </View>
+                    {isOwner && (
+                        <Button 
+                            title="Delete group"
+                            onPress={confirmAndDeleteGroup}
+                            accessibilityLabel='Delete group button'
+                            testID='DeleteGroupButton'
+                        />
+                    )}
+                    <Button 
+                        title="Generate join code"
+                        onPress={generateJoinCode}
+                        accessibilityLabel='Generate join code button'
+                        testID='GenerateJoinCodeButton'
+                    />
+                    <Button
+                        title="Go back to Group Home"
+                        onPress={navigateGroupHome}
+                        accessibilityLabel='Go back to Group Home button'
+                        testID='GoBackToGroupHomeButton'
+                    />
+                    
+                    <View style={stylesGroupSettingsController.containerUsers}>
+                        {groupUsers.map((user : User) => (
+                            <TouchableOpacity 
+                                key={user.id} 
+                                onPress={() => console.log('User pressed:', user.username)}
+                            >
+                                <Text>{user.username}</Text>
+                            </TouchableOpacity>
+                        ))}   
+                    </View>
 
-                <Dialog.Container visible={dialogVisible}>
-                    <Dialog.Title>Join Code</Dialog.Title>
-                    <Dialog.Description>
-                        {joinCode}
-                    </Dialog.Description>
-                    <Dialog.Button label="Close" onPress={closeDialog} />
-                    <Dialog.Button label="Copy" onPress={copyJoinCodeToClipboard} />
-                </Dialog.Container>
+                    <Dialog.Container visible={dialogVisible}>
+                        <Dialog.Title>Join Code</Dialog.Title>
+                        <Dialog.Description>
+                            {joinCode}
+                        </Dialog.Description>
+                        <Dialog.Button label="Close" onPress={closeDialog} />
+                        <Dialog.Button label="Copy" onPress={copyJoinCodeToClipboard} />
+                    </Dialog.Container>
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
---
+++ ReactNativeApp/src/styles/stylesGroupSettingsController.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+
+const stylesGroupSettingsController = StyleSheet.create({
+    containerUsers: {
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 2,        
+        height: 300,
+    },
+});
+
+export default stylesGroupSettingsController;
