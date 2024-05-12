7d4fbf8a1409980e607e40f01539eb91455ec58e
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-12 21:38:55 +0200

    Update useRegisterController.test.tsx

--- ReactNativeApp/src/screens/preLogin/hooks/useRegisterController.test.tsx
+++ ReactNativeApp/src/screens/preLogin/hooks/useRegisterController.test.tsx
@@ -52,177 +52,179 @@ describe('useRegisterController', () => {
         expect(result.current.lastName).toBe('newLastName');
     });
 
-    it('should display an alert when username, email, password, or confirmPassword are empty', async () => {
-        const alertSpy = jest.spyOn(Alert, 'alert');
+    describe('handleRegisterSubmit', () => {
+        it('should display an alert when username, email, password, or confirmPassword are empty', async () => {
+            const alertSpy = jest.spyOn(Alert, 'alert');
 
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('');
-            result.current.setEmail('');
-            result.current.setPassword('');
-            result.current.setConfirmPassword('');
-        });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(alertSpy).toHaveBeenCalledWith('Error', 'Username, email, password and confirmation password must be filled');
-    });
-    
-    it('should display an alert when passwords do not match', async () => {
-        const alertSpy = jest.spyOn(Alert, 'alert');
-
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('differentPassword');
-        });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(alertSpy).toHaveBeenCalledWith('Error', 'Passwords do not match. Please enter matching passwords.');
-    });
-
-    it('should display an alert when email format is not valid', async () => {
-        const alertSpy = jest.spyOn(Alert, 'alert');
-
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('newPassword');
-        });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(alertSpy).toHaveBeenCalledWith('Error', 'Invalid email format');
-    });
-
-    it('should handle register submit with valid username, email, password, and confirmPassword', async () => {
-        //mockSuccesfulFetch({ id: 'dummy', token: 'dummy_token', username: 'newUsername' });
-        mockSuccesfulFetch({});
-
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail@email.com');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('newPassword');
-        });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(fetch).toHaveBeenCalledWith(
-            `${BASE_URL}/api/users/register`,
-            expect.objectContaining({
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username: 'newUsername', email: 'newEmail@email.com', password: 'newPassword', confirmPassword: 'newPassword'}),
-            })
-        );
-    });
-
-    it('should navigate to HomeScreen when response is ok', async () => {
-        mockSuccesfulFetch({});
-    
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail@email.com');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('newPassword');
-        });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(mockedNavigate).toHaveBeenCalledWith('HomeScreen'); 
-    });
-
-    it('should update UserContext with id and username when login is successful', async () => {
-        mockSuccesfulFetch({ id: 'dummy', username: 'newUsername'});
+            const { result } = renderTestHookTest();
         
-        const mockSetId = jest.fn();
-        const mockSetContextUsername = jest.fn();
-    
-        const useContextSpy = jest.spyOn(React, 'useContext');
-        useContextSpy.mockReturnValue({ setId: mockSetId, setContextUsername: mockSetContextUsername });
-    
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail@email.com');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('newPassword');
+            act(() => {
+                result.current.setUsername('');
+                result.current.setEmail('');
+                result.current.setPassword('');
+                result.current.setConfirmPassword('');
+            });
+        
+            await act(async () => {
+                await result.current.handleRegisterSubmit();
+            });
+        
+            expect(alertSpy).toHaveBeenCalledWith('Error', 'Username, email, password and confirmation password must be filled');
         });
-    
-        await act(async () => {
-            result.current.handleRegisterSubmit();
+        
+        it('should display an alert when passwords do not match', async () => {
+            const alertSpy = jest.spyOn(Alert, 'alert');
+
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('differentPassword');
+            });
+        
+            await act(async () => {
+                await result.current.handleRegisterSubmit();
+            });
+        
+            expect(alertSpy).toHaveBeenCalledWith('Error', 'Passwords do not match. Please enter matching passwords.');
         });
-    
-        expect(mockSetId).toHaveBeenCalledWith('dummy');
-        expect(mockSetContextUsername).toHaveBeenCalledWith('newUsername');
-    
-        useContextSpy.mockRestore();
+
+        it('should display an alert when email format is not valid', async () => {
+            const alertSpy = jest.spyOn(Alert, 'alert');
+
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('newPassword');
+            });
+        
+            await act(async () => {
+                await result.current.handleRegisterSubmit();
+            });
+        
+            expect(alertSpy).toHaveBeenCalledWith('Error', 'Invalid email format');
+        });
+
+        it('should handle register submit with valid username, email, password, and confirmPassword', async () => {
+            mockSuccesfulFetch({});
+
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail@email.com');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('newPassword');
+            });
+        
+            await act(async () => {
+                await result.current.handleRegisterSubmit();
+            });
+        
+            expect(fetch).toHaveBeenCalledWith(
+                `${BASE_URL}/api/users/register`,
+                expect.objectContaining({
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username: 'newUsername', email: 'newEmail@email.com', password: 'newPassword', confirmPassword: 'newPassword'}),
+                })
+            );
+        });
+
+        it('should navigate to HomeScreen when response is ok', async () => {
+            mockSuccesfulFetch({});
+        
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail@email.com');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('newPassword');
+            });
+        
+            await act(async () => {
+                await result.current.handleRegisterSubmit();
+            });
+        
+            expect(mockedNavigate).toHaveBeenCalledWith('HomeScreen'); 
+        });
+
+        it('should update UserContext with id and username when login is successful', async () => {
+            mockSuccesfulFetch({ id: 'dummy', username: 'newUsername'});
+            
+            const mockSetId = jest.fn();
+            const mockSetContextUsername = jest.fn();
+        
+            const useContextSpy = jest.spyOn(React, 'useContext');
+            useContextSpy.mockReturnValue({ setId: mockSetId, setContextUsername: mockSetContextUsername });
+        
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail@email.com');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('newPassword');
+            });
+        
+            await act(async () => {
+                result.current.handleRegisterSubmit();
+            });
+        
+            expect(mockSetId).toHaveBeenCalledWith('dummy');
+            expect(mockSetContextUsername).toHaveBeenCalledWith('newUsername');
+        
+            useContextSpy.mockRestore();
+        });
+
+        it('should display alert when response is not ok', async () => {
+            mockFailedFetch('Invalid credentials');
+            const alertSpy = jest.spyOn(Alert, 'alert');
+        
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.setUsername('newUsername');
+                result.current.setEmail('newEmail@email.com');
+                result.current.setPassword('newPassword');
+                result.current.setConfirmPassword('newPassword');
+            });
+        
+            await act(async () => {
+                result.current.handleRegisterSubmit();
+            });
+        
+            expect(alertSpy).toHaveBeenCalledWith('Error', 'Invalid credentials');
+        });
     });
 
-    
-    it('should display alert when response is not ok', async () => {
-        mockFailedFetch('Invalid credentials');
-        const alertSpy = jest.spyOn(Alert, 'alert');
-    
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.setUsername('newUsername');
-            result.current.setEmail('newEmail@email.com');
-            result.current.setPassword('newPassword');
-            result.current.setConfirmPassword('newPassword');
+    describe('navigate', () => {
+        it('should navigate to LoginScreen', async () => {
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.navigateLoginScreen();
+            });
+        
+            expect(mockedNavigate).toHaveBeenCalledWith('LoginScreen');
         });
-    
-        await act(async () => {
-            await result.current.handleRegisterSubmit();
-        });
-    
-        expect(alertSpy).toHaveBeenCalledWith('Error', 'Invalid credentials');
-    });
 
-    it('should navigate to LoginScreen', async () => {
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.navigateLoginScreen();
+        it('should navigate to ResetPasswordScreen', async () => {
+            const { result } = renderTestHookTest();
+        
+            act(() => {
+                result.current.navigateResetPasswordScreen();
+            });
+        
+            expect(mockedNavigate).toHaveBeenCalledWith('ResetPasswordScreen');
         });
-    
-        expect(mockedNavigate).toHaveBeenCalledWith('LoginScreen');
-    });
-
-    it('should navigate to ResetPasswordScreen', async () => {
-        const { result } = renderTestHookTest();
-    
-        act(() => {
-            result.current.navigateResetPasswordScreen();
-        });
-    
-        expect(mockedNavigate).toHaveBeenCalledWith('ResetPasswordScreen');
     });
 });
