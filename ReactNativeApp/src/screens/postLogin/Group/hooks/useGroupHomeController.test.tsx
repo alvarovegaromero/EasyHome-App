b100ed376771d3cb016360faa83a7052aaaeaa2e
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-12 18:33:00 +0200

    Tests added for useGroupHomeController.test

---
+++ ReactNativeApp/src/screens/postLogin/Group/hooks/useGroupHomeController.test.tsx
@@ -0,0 +1,98 @@
+import { renderHook, waitFor } from "@testing-library/react-native";
+import useGroupHomeController from "./useGroupHomeController";
+import React from "react";
+import { mockSuccesfulFetch } from "../../../../utils/utilsTestingHooks";
+import { BASE_URL } from "../../../../config";
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+    getItem: jest.fn(() => Promise.resolve('dummy_token')),
+}));
+
+jest.mock('react-native/Libraries/Alert/Alert', () => ({
+    alert: jest.fn(),
+}));
+
+jest.mock('react-native/Libraries/Alert/Alert', () => ({
+    alert: jest.fn(),
+}));
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+    const actualNav = jest.requireActual('@react-navigation/native');
+    return {
+        ...actualNav,
+        useNavigation: () => ({
+            navigate: mockedNavigate,
+        }),
+    };
+});
+
+const renderTestHookTest = () => {
+    return renderHook(() => useGroupHomeController());
+};
+
+describe('useGroupHomeController', () => {
+    it('should call proper endpoint for fetching groups', async () => {
+        mockSuccesfulFetch({});
+
+        const mockGroupId = 'dummy_id';
+
+        const useContextSpy = jest.spyOn(React, 'useContext');
+        useContextSpy.mockReturnValue({ groupId: mockGroupId });
+
+        renderTestHookTest();
+
+        await waitFor(() => {
+            expect(fetch).toHaveBeenCalledWith(
+                `${BASE_URL}/api/groups/${mockGroupId}`,
+                expect.objectContaining({
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Token dummy_token',
+                    },
+                })
+            );
+        });
+
+        useContextSpy.mockRestore(); 
+    });
+
+    it('should fetch group data', async () => {
+        const mockGroupData = { id: 1, name: 'dummy_name', 
+                            description: 'dummy_description', currency: 'dummy_currency', 
+                            creation_date: 'dummy_creation_date', owner: 'dummy_owner'};
+
+        mockSuccesfulFetch(mockGroupData);
+
+        const { result } = renderTestHookTest();
+        
+        await waitFor(() => expect(result.current.groupName).toBe(mockGroupData.name));
+    
+        
+    });
+
+    it('should navigate to GroupSettingsScreen', () => {
+        const { result } = renderTestHookTest();
+        result.current.navigateSettings();
+        expect(mockedNavigate).toHaveBeenCalledWith('GroupSettingsScreen');
+    });
+
+    it('should navigate to HomeScreen', () => {
+        const { result } = renderTestHookTest();
+        result.current.navigateHome();
+        expect(mockedNavigate).toHaveBeenCalledWith('HomeScreen');
+    });
+
+    it('should update groupId when navigating to HomeScreen', () => {
+        const mockSetGroupId = jest.fn();
+        const useContextSpy = jest.spyOn(React, 'useContext');
+        useContextSpy.mockReturnValue({ setGroupId: mockSetGroupId });
+
+        const { result } = renderTestHookTest();
+        result.current.navigateHome();
+
+        expect(mockSetGroupId).toHaveBeenCalledWith('');
+    });
+});
