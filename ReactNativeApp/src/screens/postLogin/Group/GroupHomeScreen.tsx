4efb885ebefce77f1a1f44c49546b4381f8a08e9
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-12 16:30:39 +0200

    Styles applied to GroupHomeScreen

--- ReactNativeApp/src/screens/postLogin/Group/GroupHomeScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/GroupHomeScreen.tsx
@@ -1,6 +1,7 @@
 import { Button, SafeAreaView, ScrollView, Text, View } from "react-native";
 import useGroupHomeController from "./hooks/useGroupHomeController";
 import generalStyles from "../../../styles/styles";
+import stylesGroupHomeScreen from "../../../styles/stylesGroupHomeScreen";
 
 
 const GroupHomeScreen: React.FunctionComponent = () => {
@@ -9,50 +10,102 @@ const GroupHomeScreen: React.FunctionComponent = () => {
     return (
         <SafeAreaView style={generalStyles.defaultSafeAreaView}>
             <ScrollView style={generalStyles.defaultScrollView}>
-                <Text>{groupName}</Text>
-                <Text> What do you want to do today?</Text>
-                <Button
-                    title = "Distribute Expenses"
-                    onPress={() => {}}
-                    accessibilityLabel='Distribute expenses button'
-                    testID='DistributeExpensesButton'
-                />
-                <Button
-                    title="Manage Shopping Lists"
-                    onPress={() => {}}
-                    accessibilityLabel='Manage shopping lists button'
-                    testID='ManageShoppingListsButton'
-                />
-                <Button 
-                    title="Distribute household chores"
-                    onPress={() => {}}
-                    accessibilityLabel='Distribute household chores button'
-                    testID='DistributeHouseholdChoresButton'
-                />
-                <Button
-                    title="View the pantry"
-                    onPress={() => {}}
-                    accessibilityLabel='View the pantry button'
-                    testID='ViewThePantryButton'
-                />
-                <Button 
-                    title="Group Board"
-                    onPress={() => {}}
-                    accessibilityLabel='Group Board button'
-                    testID='GroupBoardButton'
-                />
-                <Button 
-                    title="Group Settings"
-                    onPress={navigateSettings}
-                    accessibilityLabel='Group Settings button'
-                    testID='GroupSettingsButton'
-                />
-                <Button 
-                    title="Go back to Home"
-                    onPress={navigateHome}
-                    accessibilityLabel='Go back to Home button'
-                    testID='GoBackToHomeButton'
-                />
+                <View style={generalStyles.defaultContainerScreen}>
+
+                    <Text style={stylesGroupHomeScreen.headerText}>{groupName}</Text>
+                    <Text style={generalStyles.defaultSubHeader}> What do you want to do today?</Text>
+                    
+                    <View style={stylesGroupHomeScreen.containerButtons}>
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button
+                                    title = "Distribute Expenses"
+                                    onPress={() => {}}
+                                    accessibilityLabel='Distribute expenses button'
+                                    testID='DistributeExpensesButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button
+                                    title="Manage Shopping Lists"
+                                    onPress={() => {}}
+                                    accessibilityLabel='Manage shopping lists button'
+                                    testID='ManageShoppingListsButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Distribute household chores"
+                                    onPress={() => {}}
+                                    accessibilityLabel='Distribute household chores button'
+                                    testID='DistributeHouseholdChoresButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Distribute household chores"
+                                    onPress={() => {}}
+                                    accessibilityLabel='Distribute household chores button'
+                                    testID='DistributeHouseholdChoresButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button
+                                    title="View the pantry"
+                                    onPress={() => {}}
+                                    accessibilityLabel='View the pantry button'
+                                    testID='ViewThePantryButton'
+                                />
+                            </View> 
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Group Board"
+                                    onPress={() => {}}
+                                    accessibilityLabel='Group Board button'
+                                    testID='GroupBoardButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Group Settings"
+                                    onPress={navigateSettings}
+                                    accessibilityLabel='Group Settings button'
+                                    testID='GroupSettingsButton'
+                                />
+                            </View>
+                        </View>
+
+                        <View style={stylesGroupHomeScreen.containerButton}>
+                            <View style={generalStyles.defaultButton}>
+                                <Button 
+                                    title="Go back to Home"
+                                    onPress={navigateHome}
+                                    accessibilityLabel='Go back to Home button'
+                                    testID='GoBackToHomeButton'
+                                />
+                            </View>
+                        </View>
+                    </View>
+
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
---
+++ ReactNativeApp/src/styles/stylesGroupHomeScreen.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import generalStyles from './styles';
+
+
+const stylesGroupSettingsController = StyleSheet.create({
+    headerText: {
+        ...generalStyles.defaultHeader,
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    containerButtons: {
+        marginTop: 20,
+    },
+    containerButton: {
+        ...generalStyles.defaultContainerButton,
+        marginTop: 15,
+        marginBottom: 15,
+    },
+});
+
+export default stylesGroupSettingsController;
