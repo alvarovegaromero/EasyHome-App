f34e9b3f1db5f974c5ca4c63f9733829c6ba000f
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-20 20:42:55 +0200

    Added dummy code to the new screen

---
+++ ReactNativeApp/src/screens/postLogin/Group/Board/GroupBoardScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import generalStyles from '../../../../styles/styles';
+
+const GroupBoardScreen: React.FunctionComponent = () => {
+    return (
+        <View>
+            <Text>GroupBoardScreen</Text>
+        </View>
+    );
+};
+
+export default GroupBoardScreen;
