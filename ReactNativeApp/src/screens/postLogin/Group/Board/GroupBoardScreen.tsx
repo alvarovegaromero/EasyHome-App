6ca04290281662f1b5b2045531a896cdfa4f568d
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-20 21:56:43 +0200

    Add button for navigating back in the screen

--- ReactNativeApp/src/screens/postLogin/Group/Board/GroupBoardScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Board/GroupBoardScreen.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import generalStyles from '../../../../styles/styles';
+import useGroupBoardController from "./hooks/useGroupBoardController";
 
 const GroupBoardScreen: React.FunctionComponent = () => {
+    const {navigateGroupHome} = useGroupBoardController();
+
     return (
         <View>
             <Text>GroupBoardScreen</Text>
+            <Button
+                title="Go to Group Home"
+                onPress={navigateGroupHome}
+                accessibilityLabel='Go to Group Home button'
+                testID='GoToGroupHomeButton'
+            />
         </View>
     );
 };
