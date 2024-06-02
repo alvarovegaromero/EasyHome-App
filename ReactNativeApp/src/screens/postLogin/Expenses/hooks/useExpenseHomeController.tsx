b4ebd4645b4868e8653c3d84d952dccdc5bd28eb
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-02 14:58:16 +0200

    Added code for fetching group expenses

--- ReactNativeApp/src/screens/postLogin/Expenses/hooks/useExpenseHomeController.tsx
+++ ReactNativeApp/src/screens/postLogin/Expenses/hooks/useExpenseHomeController.tsx
@@ -1,4 +1,50 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert} from 'react-native';
+import {BASE_URL} from '../../../../config';
+import {Expense} from '../types';
+import {useEffect, useState, useContext} from 'react';
+import {GroupContext} from '../../../../contexts/GroupContext';
+
 const useExpensesHomeController = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const {groupId} = useContext(GroupContext);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const fetchExpenses = async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(BASE_URL + '/api/expense_distribution/' + groupId + '/expenses', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then((data: Expense[]) => {
+        const expensesWithDates = data.map(expense => ({
+          ...expense,
+          date_paid: new Date(expense.date_paid),
+        }));
+        setExpenses(expensesWithDates);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
   return {};
 };
 
---
+++ ReactNativeApp/src/screens/postLogin/Expenses/types.ts
@@ -0,0 +1,7 @@
+export type Expense = {
+  id: number;
+  name: string;
+  amount: number;
+  date_paid: Date;
+  paid_by_username: string;
+};
