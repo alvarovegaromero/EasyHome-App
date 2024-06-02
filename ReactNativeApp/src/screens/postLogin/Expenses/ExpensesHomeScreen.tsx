94c1a830f65f4861fc8e3984174ddaa58ddb837e
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-02 12:26:31 +0200

    Add dummy code to check the screen

---
+++ ReactNativeApp/src/screens/postLogin/Expenses/ExpensesHomeScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const ExpensesHomeScreen: React.FunctionComponent = () => {
+  return (
+    <View>
+      <Text>ExpensesHomeScreen</Text>
+    </View>
+  );
+};
+
+export default ExpensesHomeScreen;
