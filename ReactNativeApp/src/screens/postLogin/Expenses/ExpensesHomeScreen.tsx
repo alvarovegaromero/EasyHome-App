724c4ede57d4416b8df03b945192a2d7ce585bfc
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-02 13:03:20 +0200

    Controller created for ExpenseHome

--- ReactNativeApp/src/screens/postLogin/Expenses/ExpensesHomeScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Expenses/ExpensesHomeScreen.tsx
@@ -1,6 +1,9 @@
 import {Text, View} from 'react-native';
+import useExpensesHomeController from './hooks/useExpenseHomeController';
 
 const ExpensesHomeScreen: React.FunctionComponent = () => {
+  const {} = useExpensesHomeController();
+
   return (
     <View>
       <Text>ExpensesHomeScreen</Text>
