99c270d345aaa55aec051f9920ec94519cdcf916
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-02 16:44:48 +0200

    Add navigation to useExpenseHomeController

--- ReactNativeApp/src/screens/postLogin/Expenses/hooks/useExpenseHomeController.tsx
+++ ReactNativeApp/src/screens/postLogin/Expenses/hooks/useExpenseHomeController.tsx
@@ -4,8 +4,11 @@ import {BASE_URL} from '../../../../config';
 import {Expense} from '../types';
 import {useEffect, useState, useContext} from 'react';
 import {GroupContext} from '../../../../contexts/GroupContext';
+import {useNavigation} from '@react-navigation/native';
 
 const useExpensesHomeController = () => {
+  const navigation = useNavigation();
+
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const {groupId} = useContext(GroupContext);
 
@@ -43,6 +46,18 @@ const useExpensesHomeController = () => {
       .catch(error => {
         console.error('Error:', error);
       });
+
+    const navigateDetailExpense = (id: string) => {
+      //navigation.navigate('DetailExpenseScreen' as never);
+    };
+
+    const navigateAddExpense = () => {
+      navigation.navigate('AddExpenseScreen' as never);
+    };
+
+    const navigateGroupHome = () => {
+      navigation.navigate('GroupHomeScreen' as never);
+    };
   };
 
   return {expenses};
