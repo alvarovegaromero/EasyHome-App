dc2e100df64f9d9f49c45dd762ff2468a077c213
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-06 19:10:22 +0200

    Added logic to fetch current task and add conditions to have view and edit mode

--- ReactNativeApp/src/screens/postLogin/Group/Chores/EstablishChoresScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Chores/EstablishChoresScreen.tsx
@@ -1,12 +1,53 @@
 import {Text, View} from 'react-native';
 import useEstablishChoresController from './hooks/useEstablishChoresController';
+import {Icon} from '@rneui/themed';
+import {TextInput} from 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const ChoresHomeScreen: React.FunctionComponent = () => {
-  const {} = useEstablishChoresController();
+  const {tasks, editMode, changeToEditMode, changeToViewMode} =
+    useEstablishChoresController();
 
   return (
     <View>
       <Text>Establish Chores Screen</Text>
+
+      <GestureHandlerRootView>
+        {!editMode ? (
+          tasks === undefined ? (
+            <Text>Loading...</Text>
+          ) : (
+            <>
+              {tasks.length === 0 ? (
+                <Text>No tasks</Text>
+              ) : (
+                tasks.map(task => <Text key={task.id}>{task.title}</Text>)
+              )}
+              <Icon
+                reverse
+                reverseColor="white"
+                name="pencil"
+                type="material-community"
+                color="#2196F3"
+                onPress={changeToEditMode}
+              />
+            </>
+          )
+        ) : (
+          <>
+            <Text>Edit mode</Text>
+            {tasks?.map(task => <TextInput key={task.id} value={task.title} />)}
+            <Icon
+              reverse
+              reverseColor="white"
+              name="content-save"
+              type="material-community"
+              color="#2196F3"
+              onPress={changeToViewMode}
+            />
+          </>
+        )}
+      </GestureHandlerRootView>
     </View>
   );
 };
--- ReactNativeApp/src/screens/postLogin/Group/Chores/hooks/useEstablishChoresController.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Chores/hooks/useEstablishChoresController.tsx
@@ -1,5 +1,58 @@
+import {useContext, useEffect, useState} from 'react';
+import {GroupContext} from '../../../../../contexts/GroupContext';
+import {Alert} from 'react-native';
+import {BASE_URL} from '../../../../../config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const useEstablishChoresController = () => {
-  return {};
+  const {groupId} = useContext(GroupContext);
+
+  const [editMode, setEditMode] = useState(false);
+  const [tasks, setTasks] = useState<any[] | undefined>(undefined);
+
+  useEffect(() => {
+    fetchTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchTasks = async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(BASE_URL + '/api/household_chores/' + groupId + '/tasks', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => {
+        console.log(data);
+        setTasks(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const changeToEditMode = () => {
+    setEditMode(true);
+  };
+
+  const changeToViewMode = () => {
+    setEditMode(false);
+  };
+
+  return {tasks, editMode, changeToEditMode, changeToViewMode};
 };
 
 export default useEstablishChoresController;
