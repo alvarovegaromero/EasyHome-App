f2cd31383926aa4ccb8f2373159afc574d2a696b
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-07 10:41:48 +0200

    Automatic login feature added. If user was logged in before closing the app, he will be redirected to HomeScreen

--- ReactNativeApp/src/screens/preLogin/hooks/useLoginController.tsx
+++ ReactNativeApp/src/screens/preLogin/hooks/useLoginController.tsx
@@ -1,4 +1,4 @@
-import {useState, useContext} from 'react';
+import {useState, useContext, useEffect} from 'react';
 import {Alert} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {BASE_URL} from '../../../config';
@@ -12,6 +12,46 @@ const useLoginController = () => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    //TODO: add tests for this and new method
+    AsyncStorage.getItem('token').then(token => {
+      if (token !== null) {
+        getInfoAutomaticLogin();
+        navigation.navigate('HomeScreen' as never);
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getInfoAutomaticLogin = async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(BASE_URL + '/api/users/my-info', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => {
+        setId(String(data.id));
+        setContextUsername(data.username);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
 
   const handleLoginSubmit = () => {
     if (username === '' || password === '') {
