4cf0bbbd61ee0a5793c1a8c0291d43473a13f72f
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-07 19:42:41 +0200

    added style in ChoresHomeScreen

--- ReactNativeApp/src/screens/postLogin/Group/Chores/ChoresHomeScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Chores/ChoresHomeScreen.tsx
@@ -1,5 +1,7 @@
-import {Button, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import useChoresHomeController from './hooks/useChoresHomeController';
+import {Icon} from '@rneui/themed';
+import generalStyles from '../../../../styles/styles';
 
 const ChoresHomeScreen: React.FunctionComponent = () => {
   const {
@@ -12,36 +14,84 @@ const ChoresHomeScreen: React.FunctionComponent = () => {
   } = useChoresHomeController();
 
   return (
-    <View>
-      <Text>Chores Home Screen</Text>
+    <SafeAreaView style={generalStyles.defaultSafeAreaView}>
+      <ScrollView style={generalStyles.defaultScrollView}>
+        <View style={generalStyles.defaultContainerScreen}>
+          <View style={generalStyles.defaultContainerHeader}>
+            <Text style={generalStyles.defaultHeader}>Chores Home Screen</Text>
+          </View>
 
-      {isOwner && (
-        <View>
-          <Button
-            title="Establish and start Household Chores Process"
-            onPress={navigateEstablishChores}
-          />
-        </View>
-      )}
+          <View
+            style={{
+              flexDirection: 'column',
+              alignItems: 'center',
+              marginTop: 20,
+            }}>
+            {isOwner && (
+              <View>
+                <Icon
+                  name="pencil"
+                  reverse
+                  reverseColor="white"
+                  type="material-community"
+                  color="#2196F3"
+                  accessibilityLabel="Add and Edit task"
+                  onPress={navigateEstablishChores}
+                  size={50}
+                />
+              </View>
+            )}
 
-      {isActivated ? (
-        <View>
-          <Button
-            title="View today's Houselhold Chores"
-            onPress={navigateViewTodaysChores}
-          />
-          <Button title="Stats" onPress={navigateStats} />
-        </View>
-      ) : (
-        <View>
-          <Text> The owner must start the process first! </Text>
-        </View>
-      )}
+            {!isActivated && (
+              <View>
+                <Text style={{marginBottom: 10, marginTop: 10, fontSize: 20}}>
+                  {' '}
+                  The owner must start the process first!{' '}
+                </Text>
+              </View>
+            )}
 
-      <View>
-        <Button title="Go Back" onPress={navigateGroupHome} />
-      </View>
-    </View>
+            <View>
+              <Icon
+                name="calendar-today"
+                reverse
+                reverseColor="white"
+                type="material-community"
+                color="#2196F3"
+                accessibilityLabel="See today's tasks"
+                onPress={navigateViewTodaysChores}
+                size={50}
+                disabled={!isActivated}
+              />
+              <Icon
+                name="chart-box"
+                reverse
+                reverseColor="white"
+                type="material-community"
+                color="#2196F3"
+                accessibilityLabel="See stats"
+                onPress={navigateStats}
+                size={50}
+                disabled={!isActivated}
+              />
+            </View>
+
+            <View>
+              <Icon
+                name="arrow-left-circle"
+                reverse
+                reverseColor="white"
+                type="material-community"
+                color="#2196F3"
+                accessibilityLabel="Go back to Group Home Screen"
+                onPress={navigateGroupHome}
+                size={50}
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
