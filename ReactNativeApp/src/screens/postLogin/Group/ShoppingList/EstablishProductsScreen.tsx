c63a8bd72c217cbca48723a6e0c18d078020b27d
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-08 19:57:09 +0200

    Added same code structure than with tasks

--- ReactNativeApp/src/screens/postLogin/Group/ShoppingList/EstablishProductsScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/ShoppingList/EstablishProductsScreen.tsx
@@ -1,15 +1,122 @@
-import {ScrollView, SafeAreaView, Text, View} from 'react-native';
+import {ScrollView, SafeAreaView, Text, View, TextInput} from 'react-native';
 import useEstablishProducts from './hooks/useEstablishProducts';
 import generalStyles from '../../../../styles/styles';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Icon} from '@rneui/themed';
+import stylesEstablishChoresScreen from '../../../../styles/stylesEstablishChoresScreen';
 
 const EstablishProductsScreen: React.FunctionComponent = () => {
-  const {} = useEstablishProducts();
+  const {
+    products,
+    addMode,
+    addInput,
+    setAddInput,
+    createProduct,
+    confirmAndDeleteProduct,
+    changeToViewMode,
+    changeToAddMode,
+  } = useEstablishProducts();
 
   return (
     <SafeAreaView style={generalStyles.defaultSafeAreaView}>
       <ScrollView style={generalStyles.defaultScrollView}>
-        <View>
-          <Text> Establish Shopping List Screen </Text>
+        <View style={generalStyles.defaultContainerScreen}>
+          <View style={generalStyles.defaultContainerHeader}>
+            <Text style={generalStyles.defaultHeader}>
+              Establish Shopping List Screen
+            </Text>
+          </View>
+
+          <Text style={generalStyles.defaultSubHeader}>
+            Current products added to the system:
+          </Text>
+
+          <GestureHandlerRootView>
+            {products === undefined ? (
+              <Text>Loading...</Text>
+            ) : (
+              <>
+                {products.length === 0 ? (
+                  <Text>No products found :(</Text>
+                ) : (
+                  <>
+                    {products.map(product => (
+                      <View
+                        key={product.id}
+                        style={stylesEstablishChoresScreen.containerTask}>
+                        <View
+                          style={stylesEstablishChoresScreen.containerTextTask}>
+                          <Text
+                            style={stylesEstablishChoresScreen.styleTextTask}>
+                            {product.name}
+                          </Text>
+                        </View>
+                        <View
+                          style={stylesEstablishChoresScreen.containerIconTask}>
+                          <Icon
+                            name="delete"
+                            type="material-community"
+                            color="#ff4d4d"
+                            accessibilityLabel="Delete product"
+                            onPress={() => {
+                              confirmAndDeleteProduct(product.id);
+                            }}
+                            size={40}
+                          />
+                        </View>
+                      </View>
+                    ))}
+                  </>
+                )}
+
+                {addMode ? (
+                  <>
+                    <View style={generalStyles.defaultInput}>
+                      <TextInput value={addInput} onChangeText={setAddInput} />
+                    </View>
+                    <View
+                      style={
+                        stylesEstablishChoresScreen.containerSaveCancelNewTask
+                      }>
+                      <Icon
+                        reverse
+                        reverseColor="white"
+                        name="content-save"
+                        type="material-community"
+                        color="#2196F3"
+                        accessibilityLabel="Save new products"
+                        onPress={createProduct}
+                        size={40}
+                      />
+                      <Icon
+                        reverse
+                        reverseColor="white"
+                        name="close"
+                        type="material-community"
+                        color="#2196F3"
+                        accessibilityLabel="Cancel adding a new product"
+                        onPress={changeToViewMode}
+                        size={40}
+                      />
+                    </View>
+                  </>
+                ) : (
+                  <>
+                    <Icon
+                      reverse
+                      reverseColor="white"
+                      name="plus"
+                      type="material-community"
+                      color="#2196F3"
+                      accessibilityLabel="Change to Add Mode"
+                      onPress={changeToAddMode}
+                      size={30}
+                    />
+                  </>
+                )}
+              </>
+            )}
+          </GestureHandlerRootView>
         </View>
       </ScrollView>
     </SafeAreaView>
--- ReactNativeApp/src/screens/postLogin/Group/ShoppingList/hooks/useEstablishProducts.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/ShoppingList/hooks/useEstablishProducts.tsx
@@ -1,5 +1,155 @@
+import {useContext, useEffect, useState} from 'react';
+import {GroupContext} from '../../../../../contexts/GroupContext';
+import {Alert} from 'react-native';
+import {BASE_URL} from '../../../../../config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const useEstablishProducts = () => {
-  return {};
+  const {groupId} = useContext(GroupContext);
+
+  const [products, setProducts] = useState<any[] | undefined>(undefined);
+
+  const [addMode, setAddMode] = useState(false);
+  const [addInput, setAddInput] = useState('');
+
+  useEffect(() => {
+    fetchProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchProducts = async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(BASE_URL + '/api/shopping_list/' + groupId + '/products', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const createProduct = async () => {
+    if (addInput === '') {
+      Alert.alert('Error', 'Name of the new product must be filled');
+      console.error('Creation Failed - name of the new product must be filled');
+      return;
+    }
+
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(BASE_URL + '/api/shopping_list/' + groupId + '/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify({name: addInput}),
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then(() => {
+        fetchProducts();
+        setAddInput('');
+        changeToViewMode();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const confirmAndDeleteProduct = (productId: number) => {
+    Alert.alert(
+      'Delete Product',
+      `Are you sure you want to delete this product?\n\nAll the products bought` +
+        `will be deleted.`,
+      [
+        {
+          text: 'No',
+          style: 'cancel',
+        },
+        {
+          text: 'Yes',
+          onPress: () => deleteProduct(productId),
+        },
+      ],
+    );
+  };
+
+  const deleteProduct = async (productId: number) => {
+    const token = await AsyncStorage.getItem('token');
+
+    fetch(
+      BASE_URL +
+        '/api/shopping_list/' +
+        groupId +
+        '/products/' +
+        productId.toString(),
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`,
+        },
+      },
+    )
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(({error}) => {
+            Alert.alert('Error', error);
+            throw new Error(`${response.status} - ${error}`);
+          });
+        } else {
+          fetchProducts();
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const changeToViewMode = () => {
+    setAddMode(false);
+  };
+
+  const changeToAddMode = () => {
+    setAddMode(true);
+  };
+
+  return {
+    products,
+    addMode,
+    addInput,
+    setAddInput,
+    createProduct,
+    confirmAndDeleteProduct,
+    changeToViewMode,
+    changeToAddMode,
+  };
 };
 
 export default useEstablishProducts;
