69d22742c9dedae20c0cc9290dc122bd5cc0de2c
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-09 18:46:22 +0200

    Add styles to footer. Using 7 icons

--- ReactNativeApp/src/utils/GroupFooter/GroupFooter.tsx
+++ ReactNativeApp/src/utils/GroupFooter/GroupFooter.tsx
@@ -23,68 +23,82 @@ const GroupFooter: React.FunctionComponent<GroupFooterProps> = ({
 
   return (
     <View style={stylesGroupFooter.containerFooter}>
-      <Icon
-        name="currency-usd"
-        type="material-community"
-        color={activeSection === 'Expenses' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Expenses"
-        onPress={navigateToExpenses}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="currency-usd"
+          type="material-community"
+          color={activeSection === 'Expenses' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Expenses"
+          onPress={navigateToExpenses}
+          size={40}
+        />
+      </View>
 
-      <Icon
-        name="cart-plus"
-        type="material-community"
-        color={activeSection === 'ShoppingList' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Shopping List"
-        onPress={navigateToShoppingList}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="cart-plus"
+          type="material-community"
+          color={activeSection === 'ShoppingList' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Shopping List"
+          onPress={navigateToShoppingList}
+          size={40}
+        />
+      </View>
 
-      <Icon
-        name="broom"
-        type="material-community"
-        color={activeSection === 'Chores' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Chores"
-        onPress={navigateToChores}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="broom"
+          type="material-community"
+          color={activeSection === 'Chores' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Chores"
+          onPress={navigateToChores}
+          size={40}
+        />
+      </View>
 
-      <Icon
-        name="home"
-        type="material-community"
-        color={activeSection === 'GroupHome' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Group Home"
-        onPress={navigateToGroupHome}
-        size={40}
-      />
+      <View style={stylesGroupFooter.homeIcon}>
+        <Icon
+          name="home"
+          type="material-community"
+          color={activeSection === 'GroupHome' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Group Home"
+          onPress={navigateToGroupHome}
+          size={60}
+        />
+      </View>
 
-      <Icon
-        name="file-cabinet"
-        type="material-community"
-        color={activeSection === 'GroupPantry' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Group Pantry"
-        onPress={navigateToGroupPantry}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="file-cabinet"
+          type="material-community"
+          color={activeSection === 'GroupPantry' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Group Pantry"
+          onPress={navigateToGroupPantry}
+          size={40}
+        />
+      </View>
 
-      <Icon
-        name="bulletin-board"
-        type="material-community"
-        color={activeSection === 'Board' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Group Board"
-        onPress={navigateToGroupBoard}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="bulletin-board"
+          type="material-community"
+          color={activeSection === 'Board' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Group Board"
+          onPress={navigateToGroupBoard}
+          size={40}
+        />
+      </View>
 
-      <Icon
-        name="cog"
-        type="material-community"
-        color={activeSection === 'GroupSettings' ? '#FF7F50' : 'white'}
-        accessibilityLabel="Navigate to Group Settings"
-        onPress={navigateToGroupSettings}
-        size={40}
-      />
+      <View style={stylesGroupFooter.smallIcon}>
+        <Icon
+          name="cog"
+          type="material-community"
+          color={activeSection === 'GroupSettings' ? '#FF7F50' : 'white'}
+          accessibilityLabel="Navigate to Group Settings"
+          onPress={navigateToGroupSettings}
+          size={40}
+        />
+      </View>
     </View>
   );
 };
--- ReactNativeApp/src/utils/GroupFooter/stylesGroupFooter.ts
+++ ReactNativeApp/src/utils/GroupFooter/stylesGroupFooter.ts
@@ -4,8 +4,19 @@ const stylesGroupFooter = StyleSheet.create({
   containerFooter: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    padding: 10,
+    paddingTop: 5,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    paddingRight: 5,
     backgroundColor: '#2196F3',
+    height: 72.5,
+  },
+  smallIcon: {
+    marginTop: 15,
+  },
+
+  homeIcon: {
+    marginRight: 5,
   },
 });
 
