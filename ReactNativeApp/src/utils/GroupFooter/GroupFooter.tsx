6bb148b91ec20c8b8451bd83b7ec4aa05dcecf25
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-09 18:01:31 +0200

    Add flag to set a color to "orange" when that section is active

--- ReactNativeApp/src/utils/GroupFooter/GroupFooter.tsx
+++ ReactNativeApp/src/utils/GroupFooter/GroupFooter.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import {View} from 'react-native';
 import {Icon} from '@rneui/themed';
 import useGroupFooterController from './hooks/useGroupFooterController';
+import stylesGroupFooter from './stylesGroupFooter';
 
-const GroupFooter: React.FunctionComponent = () => {
+interface GroupFooterProps {
+  activeSection: string;
+}
+
+const GroupFooter: React.FunctionComponent<GroupFooterProps> = ({
+  activeSection,
+}) => {
   const {
     navigateToExpenses,
     navigateToChores,
@@ -15,65 +22,65 @@ const GroupFooter: React.FunctionComponent = () => {
   } = useGroupFooterController();
 
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        padding: 10,
-        backgroundColor: '#2196F3',
-      }}>
+    <View style={stylesGroupFooter.containerFooter}>
       <Icon
         name="currency-usd"
         type="material-community"
-        color="white"
+        color={activeSection === 'Expenses' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Expenses"
         onPress={navigateToExpenses}
         size={40}
       />
+
       <Icon
         name="cart-plus"
         type="material-community"
-        color="white"
+        color={activeSection === 'ShoppingList' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Shopping List"
         onPress={navigateToShoppingList}
         size={40}
       />
+
       <Icon
         name="broom"
         type="material-community"
-        color="white"
+        color={activeSection === 'Chores' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Chores"
         onPress={navigateToChores}
         size={40}
       />
+
       <Icon
         name="home"
         type="material-community"
-        color="white"
+        color={activeSection === 'GroupHome' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Group Home"
         onPress={navigateToGroupHome}
         size={40}
       />
+
       <Icon
         name="file-cabinet"
         type="material-community"
-        color="white"
+        color={activeSection === 'GroupPantry' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Group Pantry"
         onPress={navigateToGroupPantry}
         size={40}
       />
+
       <Icon
         name="bulletin-board"
         type="material-community"
-        color="white"
+        color={activeSection === 'GroupBoard' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Group Board"
         onPress={navigateToGroupBoard}
         size={40}
       />
+
       <Icon
         name="cog"
         type="material-community"
-        color="white"
+        color={activeSection === 'GroupSettings' ? '#FF7F50' : 'white'}
         accessibilityLabel="Navigate to Group Settings"
         onPress={navigateToGroupSettings}
         size={40}
---
+++ ReactNativeApp/src/utils/GroupFooter/stylesGroupFooter.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const stylesGroupFooter = StyleSheet.create({
+  containerFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+    backgroundColor: '#2196F3',
+  },
+});
+
+export default stylesGroupFooter;
