b90d9f7a68d5a2bd356b9318922a5edae0261f17
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-10 17:43:15 +0200

    Add navigation to others screens

--- ReactNativeApp/src/screens/postLogin/Group/Expenses/ExpensesHomeScreen.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Expenses/ExpensesHomeScreen.tsx
@@ -2,9 +2,15 @@ import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import useExpensesHomeController from './hooks/useExpensesHomeController';
 import generalStyles from '../../../../styles/styles';
 import GroupFooter from '../../../../utils/GroupFooter/GroupFooter';
+import {Icon} from '@rneui/themed';
 
 const ExpensesHomeScreen: React.FunctionComponent = () => {
-  const {} = useExpensesHomeController();
+  const {
+    navigateToAddExpense,
+    navigateToGroupDebts,
+    navigateToGroupExpenses,
+    navigateGroupHomeScreen,
+  } = useExpensesHomeController();
 
   return (
     <SafeAreaView style={generalStyles.defaultSafeAreaView}>
@@ -14,6 +20,52 @@ const ExpensesHomeScreen: React.FunctionComponent = () => {
             <Text style={generalStyles.defaultHeader}>
               Expense Distribution
             </Text>
+          </View>
+
+          <View>
+            <Icon
+              name="plus-thick"
+              reverse
+              reverseColor="white"
+              type="material-community"
+              color="#2196F3"
+              accessibilityLabel="Add an expense"
+              onPress={navigateToAddExpense}
+              size={50}
+            />
+
+            <Icon
+              name="currency-usd"
+              reverse
+              reverseColor="white"
+              type="material-community"
+              color="#2196F3"
+              accessibilityLabel="See group's debts"
+              onPress={navigateToGroupDebts}
+              size={50}
+            />
+
+            <Icon
+              name="notebook-outline"
+              reverse
+              reverseColor="white"
+              type="material-community"
+              color="#2196F3"
+              accessibilityLabel="See group's expenses"
+              onPress={navigateToGroupExpenses}
+              size={50}
+            />
+
+            <Icon
+              name="arrow-left-circle"
+              reverse
+              reverseColor="white"
+              type="material-community"
+              color="#2196F3"
+              accessibilityLabel="Go back to Group Home Screen"
+              onPress={navigateGroupHomeScreen}
+              size={50}
+            />
           </View>
         </View>
       </ScrollView>
--- ReactNativeApp/src/screens/postLogin/Group/Expenses/hooks/useExpensesHomeController.tsx
+++ ReactNativeApp/src/screens/postLogin/Group/Expenses/hooks/useExpensesHomeController.tsx
@@ -1,5 +1,30 @@
+import {useNavigation} from '@react-navigation/native';
+
 const useExpensesHomeController = () => {
-  return {};
+  const navigation = useNavigation();
+
+  const navigateToAddExpense = () => {
+    navigation.navigate('AddExpenseScreen' as never);
+  };
+
+  const navigateToGroupDebts = () => {
+    // navigation.navigate("GroupDebtsScreen" as never)
+  };
+
+  const navigateToGroupExpenses = () => {
+    // navigation.navigate("GroupExpensesScreen" as never)
+  };
+
+  const navigateGroupHomeScreen = () => {
+    navigation.navigate('GroupHomeScreen' as never);
+  };
+
+  return {
+    navigateToAddExpense,
+    navigateToGroupDebts,
+    navigateToGroupExpenses,
+    navigateGroupHomeScreen,
+  };
 };
 
 export default useExpensesHomeController;
