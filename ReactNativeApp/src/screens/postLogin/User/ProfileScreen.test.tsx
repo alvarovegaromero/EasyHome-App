9701ed741e258dce4d6c2da8aae63e8021b2899b
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-14 11:23:50 +0200

    Fix tests in ProfileScreen.test

--- ReactNativeApp/src/screens/postLogin/User/ProfileScreen.test.tsx
+++ ReactNativeApp/src/screens/postLogin/User/ProfileScreen.test.tsx
@@ -1,9 +1,9 @@
 import {render, fireEvent} from '@testing-library/react-native';
 import ProfileScreen from './ProfileScreen';
 import useProfileController from './hooks/useProfileController';
+import {NavigationContainer} from '@react-navigation/native';
 
 jest.mock('./hooks/useProfileController', () => {
-  const handleGoBack = jest.fn();
   const navigateEditProfileScreen = jest.fn();
 
   return () => ({
@@ -11,42 +11,46 @@ jest.mock('./hooks/useProfileController', () => {
     email: 'myEmail',
     firstName: 'myFirstName',
     lastName: 'myLastName',
-    handleGoBack,
     navigateEditProfileScreen,
   });
 });
 
+const TestComponent = () => (
+  <NavigationContainer>
+    <ProfileScreen />
+  </NavigationContainer>
+);
+
 const renderScreen = () => {
-  return render(<ProfileScreen />);
+  return render(<TestComponent />);
 };
 
 describe('ProfileScreen', () => {
   it('should render all components', () => {
     const {getByText, getByTestId} = renderScreen();
 
-    expect(getByText('My Profile:')).toBeTruthy();
+    expect(getByText('Username:')).toBeTruthy();
+    expect(getByText('Email:')).toBeTruthy();
+    expect(getByText('First Name:')).toBeTruthy();
+    expect(getByText('Last Name:')).toBeTruthy();
 
-    expect(getByText('Username: myUsername')).toBeTruthy();
-    expect(getByText('Email: myEmail')).toBeTruthy();
-    expect(getByText('First Name: myFirstName')).toBeTruthy();
-    expect(getByText('Last Name: myLastName')).toBeTruthy();
+    expect(getByText('myUsername')).toBeTruthy();
+    expect(getByText('myEmail')).toBeTruthy();
+    expect(getByText('myFirstName')).toBeTruthy();
+    expect(getByText('myLastName')).toBeTruthy();
 
-    expect(getByTestId('GoBackButton')).toBeTruthy();
     expect(getByTestId('EditProfileButton')).toBeTruthy();
   });
 
   it('should call the correct functions when buttons are pressed', () => {
     const {getByTestId} = renderScreen();
 
-    const {handleGoBack, navigateEditProfileScreen} = useProfileController();
+    const {navigateEditProfileScreen} = useProfileController();
 
-    expect(handleGoBack).not.toHaveBeenCalled();
     expect(navigateEditProfileScreen).not.toHaveBeenCalled();
 
-    fireEvent.press(getByTestId('GoBackButton'));
     fireEvent.press(getByTestId('EditProfileButton'));
 
-    expect(handleGoBack).toHaveBeenCalled();
     expect(navigateEditProfileScreen).toHaveBeenCalled();
   });
 });
